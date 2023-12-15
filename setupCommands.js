049afb4383394f58c0b93ed14f6248e4fafaff0b
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-12-16 00:55:06 +0100

    Simplify setupCommands.js (#7)

--- setupCommands.js
+++ setupCommands.js
@@ -1,97 +1,103 @@
-require("dotenv").config();
-const {
-  Client,
-  GatewayIntentBits,
-  Partials,
-  ApplicationCommandType,
-  ApplicationCommandOptionType,
-} = require("discord.js");
-const bot = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.DirectMessages,
-  ],
-  partials: [
-    Partials.User,
-    Partials.Channel,
-    Partials.GuildMember,
-    Partials.Message,
-    Partials.Reaction,
-  ],
-});
+import * as dotenv from "dotenv";
+dotenv.config();
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
+
 const token = process.env["TOKEN"];
-bot.on("ready", async () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
-  let options = [
-    {
-      name: "pokemon",
-      description: "The pokemon to translate",
-      required: true,
-      type: ApplicationCommandOptionType.String,
-      autocomplete: true,
-    },
-    {
-      name: "target",
-      description: "The language to translate to",
-      required: false,
-      type: ApplicationCommandOptionType.String,
-      choices: [
-        {
-          name: "日本語",
-          value: "1",
-        },
-        {
-          name: "Rōmaji",
-          value: "2",
-        },
-        {
-          name: "한국어",
-          value: "3",
-        },
-        {
-          name: "中国人",
-          value: "4",
-        },
-        {
-          name: "Français",
-          value: "5",
-        },
-        {
-          name: "Deutsch",
-          value: "6",
-        },
-        {
-          name: "Español",
-          value: "7",
-        },
-        {
-          name: "italiano",
-          value: "8",
-        },
-        {
-          name: "English",
-          value: "9",
-        },
-        {
-          name: "čeština",
-          value: "10",
-        },
-      ],
-    },
-  ];
-  await bot.application?.commands?.create({
-    name: "uebersetzen",
-    description: "Übersetzt den Namen des Pokemon ins Englische",
-    type: ApplicationCommandType.ChatInput,
-    options: options,
-  });
-  await bot.application?.commands?.create({
-    name: "translate",
-    description: "Translates the Name of a pokemon into english",
-    type: ApplicationCommandType.ChatInput,
-    options: options,
-  });
-  process.kill(process.pid, "SIGTERM");
-});
-bot.login(token);
+const rest = new REST().setToken(token);
+
+let commands = [
+  new SlashCommandBuilder()
+    .setName("translate")
+    .setNameLocalizations({
+      de: "uebersetzen",
+    })
+    .setDescription("Translates the Name of a pokemon into english")
+    .setDescriptionLocalizations({
+      de: "Übersetzt den Namen des Pokemon ins Englische",
+    })
+    .addStringOption((option) =>
+      option
+        .setName("pokemon")
+        .setNameLocalizations({
+          de: "pokemon",
+        })
+        .setDescription("The pokemon to translate")
+        .setDescriptionLocalizations({
+          de: "Das zu übersetzende Pokémon",
+        })
+        .setRequired(true)
+        .setAutocomplete(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("target")
+        .setNameLocalizations({
+          de: "ziel",
+        })
+        .setDescription("The language to translate to")
+        .setDescriptionLocalizations({
+          de: "Die Sprache in die übersetzt werden soll",
+        })
+        .setRequired(false)
+        .addChoices(
+          {
+            name: "日本語",
+            value: "1",
+          },
+          {
+            name: "Rōmaji",
+            value: "2",
+          },
+          {
+            name: "한국어",
+            value: "3",
+          },
+          {
+            name: "中国人",
+            value: "4",
+          },
+          {
+            name: "Français",
+            value: "5",
+          },
+          {
+            name: "Deutsch",
+            value: "6",
+          },
+          {
+            name: "Español",
+            value: "7",
+          },
+          {
+            name: "italiano",
+            value: "8",
+          },
+          {
+            name: "English",
+            value: "9",
+          },
+          {
+            name: "čeština",
+            value: "10",
+          },
+        ),
+    ),
+];
+
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`,
+    );
+    const userData = await rest.get(Routes.user());
+    const userId = userData.id;
+    const data = await rest.put(Routes.applicationCommands(userId), {
+      body: commands,
+    });
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`,
+    );
+  } catch (err) {
+    console.error(err);
+  }
+})();
